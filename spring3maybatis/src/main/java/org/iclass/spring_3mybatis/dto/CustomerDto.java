3884cdf3e7bd19a253559378e705dbe492fdded1
GAMGO <gamgo445@gmail.com>
2025-08-14 14:35:17 +0900

    0.0.0.22

--- spring3maybatis/src/main/java/org/iclass/spring_3mybatis/dto/CustomerDto.java
+++ spring3maybatis/src/main/java/org/iclass/spring_3mybatis/dto/CustomerDto.java
@@ -1,16 +1,17 @@
 package org.iclass.spring_3mybatis.dto;
 import java.sql.Date;
 
+import lombok.Builder;
 // import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @Setter
 @ToString
-// @AllArgsConstructor // 모든 프로퍼티값 초기화하는 생성자 코드
 @RequiredArgsConstructor
 public class CustomerDto {
     private final String customerId;

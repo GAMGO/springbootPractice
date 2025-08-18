b7b7c58f74017ac67dd3b641457d48beb41666bb
GAMGO <gamgo445@gmail.com>
2025-08-18 11:10:01 +0900

    0.0.0.5

--- spring_4restapi/src/main/java/org/iclass/spring_4restapi/controller/CustomerRestController.java
+++ spring_4restapi/src/main/java/org/iclass/spring_4restapi/controller/CustomerRestController.java
@@ -6,7 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import java.util.*;
 import org.springframework.http.*;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
 
 @Slf4j
 @RestController
@@ -15,35 +18,59 @@ public class CustomerRestController {
     private CustomerService service;
 
     @GetMapping("/api/customers")
-    public ResponseEntity <List<CustomerDto>> getCustomers() {
+    public ResponseEntity<List<CustomerDto>> getCustomers() {
         return ResponseEntity.ok().body(service.allCustomer());
     }
+
     @GetMapping("/api/customers/{customerid}")
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable String customerid) {
         return ResponseEntity.ok().body(service.getCustomer(customerid));
     }
+
     @PostMapping("/api/customers")
-    public ResponseEntity<?> signup(@RequestBody CustomerDto dto) {
-    try {
-         int r = service.join(dto);
-         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("success",r));
-    } catch (Exception e) {
-        log.info("Sing up Failed : {}",e.getMessage());
-        return ResponseEntity.badRequest().body(Map.of("error",e.getMessage()));
-    }         
-    }
-    @PutMapping("/api/customers/{customerid}")
-    public ResponseEntity<?> update (@PathVariable String customerid,@RequestBody CustomerDto dto) {
+    public ResponseEntity<?> signup(@Valid @RequestBody CustomerDto dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> er = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(err -> {
+                er.put(err.getField(), err.getDefaultMessage());
+            });
+        }
         try {
-            return ResponseEntity.ok().body(Map.of("success",service.update(dto)));
+            int r = service.join(dto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("success", r));
         } catch (Exception e) {
-            log.info("Update Exception : {}",e.getMessage());
-            return ResponseEntity.badRequest().body(Map.of("error",e.getMessage()));
+            log.info("Sing up Failed : {}", e.getMessage());
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
+    }
+
+    @PatchMapping("/api/customers/{customerid}")
+    public ResponseEntity<?> update(@PathVariable String customerid, @RequestBody CustomerDto dto,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> er = new HashMap<>();
+            bindingResult.getFieldErrors().forEach(err -> {
+                er.put(err.getField(), err.getDefaultMessage());
+            });
+        }
+        try {
+            int r = service.update(dto);
+            if (r == 0) {
+                throw new IllegalArgumentException("{customerid} 가 없음");
+            }
+            return ResponseEntity.ok().body(Map.of("success", service.update(dto)));
+        } catch (Exception e) {
+            log.info("Update Exception : {}", e.getMessage());
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
     }
 
     @DeleteMapping("/api/customers/{customerid}")
-        public ResponseEntity<Integer> delete(@PathVariable String customerid) {
+    public ResponseEntity<Integer> delete(@PathVariable String customerid) {
+        int r = service.delete(customerid);
+        if (r == 0) {
+            throw new IllegalArgumentException("{customerid} 가 없음");
+        }
         return ResponseEntity.ok().body(service.delete(customerid));
     }
 }
--- spring_4restapi/src/main/java/org/iclass/spring_4restapi/dto/CustomerDto.java
+++ spring_4restapi/src/main/java/org/iclass/spring_4restapi/dto/CustomerDto.java
@@ -1,6 +1,11 @@
 package org.iclass.spring_4restapi.dto;
 import java.sql.Date;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 // import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,9 +19,15 @@ import lombok.ToString;
 @ToString
 @RequiredArgsConstructor
 public class CustomerDto {
+    @NotBlank(message = "ID값 필수")
+    @Pattern(regexp = "^[A-Za-z0-9]{4,}$", message = "영문자,숫자 구성 4글자 이상")
     private final String customerId;
+    @NotBlank(message = "이름입력필수")
     private final String name;
+    @Email(message = "올바른 이메일 형식 준수")
     private final String email;
+    @Min(value = 0, message = "나이 0 이상")
+    @Max(value = 100, message = "나이 100이하")
     private final Integer age;
     private final Date regDate;
 }

a3ccfce11d902fc6a46e5256b359058660cf4231
GAMGO <gamgo445@gmail.com>
2025-08-18 15:53:54 +0900

    0.0.0.63

---
+++ spring_5webmvc/src/main/java/org/iclass/spring_5webmvc/controller/TestController_2Parameter.java
@@ -0,0 +1,16 @@
+package org.iclass.spring_5webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class TestController_2Parameter {
+    @GetMapping("/list")
+    public void list(String name, @RequestParam(defaultValue = "0") int age) {
+        log.info(name);
+        log.info("{age}",age);
+    }
+}

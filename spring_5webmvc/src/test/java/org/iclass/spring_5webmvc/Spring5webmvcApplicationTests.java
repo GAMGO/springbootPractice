6b58bf7afd4af8ce541b0e7c694a709909844bb1
GAMGO <gamgo445@gmail.com>
2025-08-19 10:00:43 +0900

    0.0.0.69

--- spring_5webmvc/src/test/java/org/iclass/spring_5webmvc/Spring5webmvcApplicationTests.java
+++ spring_5webmvc/src/test/java/org/iclass/spring_5webmvc/Spring5webmvcApplicationTests.java
@@ -1,13 +1,21 @@
 package org.iclass.spring_5webmvc;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
+@ComponentScan(basePackages ="org.iclass")
+@Slf4j
 @SpringBootTest
 class Spring5webmvcApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
